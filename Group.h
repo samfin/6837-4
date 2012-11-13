03c44e017cc50454be4f2ca2cfa1d708a271c6eb
Samuel P Fingeret <samfin@mit.edu>
2012-11-13 16:44:05 -0500

    Implemented group

--- Group.h
+++ Group.h
@@ -6,6 +6,7 @@
 #include "Ray.h"
 #include "Hit.h"
 #include <iostream>
+#include <vector>
 
 using  namespace std;
 
@@ -21,7 +22,7 @@ public:
   }
 	
   Group( int num_objects ){
-
+    objects = vector<Object3D*>(num_objects);
   }
 
   ~Group(){
@@ -29,19 +30,26 @@ public:
   }
 
   virtual bool intersect( const Ray& r , Hit& h , float tmin ) {
-		
+        bool success = 0;
+		for(int i = 0; i < objects.size(); i++) {
+		    if(objects[i])
+		        success |= objects[i]->intersect(r, h, tmin);
+		}
+		return success;
    }
 	
   void addObject( int index , Object3D* obj ){
-
+    // why do we need an index, it is never used and the vector is private
+    // oh well
+    objects[index] = obj;
   }
 
   int getGroupSize(){ 
-  
+    return objects.size();
   }
 
  private:
-
+    vector<Object3D*> objects;
 };
 
 #endif
